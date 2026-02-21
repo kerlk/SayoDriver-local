c1c5b597a69d70a79359e1d44cbacac77d5eb902
kerlk <98922638+kerlk@users.noreply.github.com>
2026-02-21 13:04:21 +0300

    Release v1

--- start.py
+++ start.py
@@ -1,8 +1,1 @@
-import subprocess, webbrowser, time, sys
-
-PORT = 80
-subprocess.Popen([sys.executable, "-m", "http.server", str(PORT)])
-time.sleep(2)
-webbrowser.open(f"http://localhost:{PORT}")
-print(f"Server running on port {PORT}")
-subprocess.wait()
+#not working for now
